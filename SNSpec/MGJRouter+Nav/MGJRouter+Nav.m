0726a9d3ef9df9311e80c8309cde2a935aff631a
Sunny <zzl@zhaioto.com>
2018-06-28 18:03:16 +0800

    "MGJRouter+Nav.h"

--- SNSpec/MGJRouter+Nav/MGJRouter+Nav.m
+++ SNSpec/MGJRouter+Nav/MGJRouter+Nav.m
@@ -6,7 +6,32 @@
 //
 
 #import "MGJRouter+Nav.h"
+#import <objc/runtime.h>
+
+static NSString *navControllerWithSetterGetterKey = @"navControllerWithSetterGetterKey";
 
 @implementation MGJRouter (Nav)
+
+-(void)setNavController:(UINavigationController *)navController{
+     objc_setAssociatedObject(self, &navControllerWithSetterGetterKey, navControllerWithSetterGetterKey, OBJC_ASSOCIATION_RETAIN);
+}
+
+-(UINavigationController *)navController {
+    return objc_getAssociatedObject(self, &navControllerWithSetterGetterKey);
+}
+
+
+
+
+//@implementation Programmer (Category)
+//
+//- (void)setNameWithSetterGetter:(NSString *)nameWithSetterGetter {
+//    objc_setAssociatedObject(self, &nameWithSetterGetterKey, nameWithSetterGetter, OBJC_ASSOCIATION_COPY);
+//}
+//- (NSString *)nameWithSetterGetter {
+//    return objc_getAssociatedObject(self, &nameWithSetterGetterKey);
+//}
+
+
 
 @end
